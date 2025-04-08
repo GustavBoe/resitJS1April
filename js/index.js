b98ad5e3f342451c74d114d7a461dafd0fd381b6
GustavBoe <gustavhugoboe@icloud.com>
2025-04-08 13:18:39 +0200

    Getting started. Fetching from Api and making the basic page, as well as the project skeleton.

---
+++ js/index.js
@@ -0,0 +1,96 @@
+const apiURL = "https://v2.api.noroff.dev/gamehub";
+
+let allProducts = [];
+async function getProducts(url) {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    allProducts = await json.data;
+  } catch (error) {
+    console.log("Something went wrong");
+  } finally {
+  }
+}
+async function addButtonClick() {
+  try {
+    await cartList.push(allProducts[product]);
+    console.log(cartList);
+  } catch (error) {
+    console.log("Something went wrong", error);
+  }
+}
+const displayContainer = document.getElementById("display-container");
+
+function generateProductHtml(product) {
+  const productContainer = document.createElement("div");
+  productContainer.classList.add("product-container");
+
+  const productTitle = document.createElement("h2");
+
+  productTitle.textContent = allProducts[product].title;
+
+  const productImage = document.createElement("img");
+  productImage.src = allProducts[product].image.url;
+  productImage.alt = allProducts[product].image.alt;
+
+  const productPrice = document.createElement("h2");
+  if (!allProducts[product].onSale) {
+    productPrice.textContent = "$" + " " + allProducts[product].price;
+  } else {
+    productPrice.textContent = "$" + " " + allProducts[product].discountedPrice;
+  }
+
+  const productLink = document.createElement("a");
+  productLink.href = `product.html?id=${allProducts[product].id}`; /* Code borrowed from Tonje Schjefstad´s "bestsellers.js"*/
+  productLink.classList.add("product-link-button");
+  productLink.innerHTML = "<button>View</button>";
+
+  async function addButtonClick() {
+    try {
+      await cartList.push(allProducts[product]);
+      console.log(cartList);
+      localStorage.setItem("cartList", JSON.stringify(cartList));
+    } catch (error) {
+      console.log("Something went wrong", error);
+    }
+  }
+
+  function removeButtonClick() {
+    cartList.pop(allProducts[product]);
+    console.log(cartList);
+  }
+  const buttonContainer = document.createElement("div");
+  buttonContainer.classList.add("button-container");
+
+  const removeButton = document.createElement("button");
+  removeButton.textContent = "-";
+  removeButton.addEventListener("click", removeButtonClick);
+
+  const addButton = document.createElement("button");
+  addButton.textContent = "+";
+  addButton.addEventListener("click", addButtonClick);
+
+  buttonContainer.append(removeButton, addButton);
+  productContainer.append(
+    productTitle,
+    productImage,
+    productPrice,
+    productLink,
+    buttonContainer
+  );
+
+  return productContainer;
+}
+
+async function displayProducts(data) {
+  for (let i = 0; i < data.length; i++) {
+    const productHtml = await generateProductHtml(i);
+    await displayContainer.append(productHtml);
+  }
+}
+
+async function main() {
+  await getProducts(apiURL);
+  await displayProducts(allProducts);
+}
+main();
